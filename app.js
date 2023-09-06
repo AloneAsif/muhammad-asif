7cfa163487689726c5f45eb36afa4372fd55af98
aloneasif <asifleon3@gmail.com>
2023-09-06 11:51:59 +0500

    javascript

---
+++ app.js
@@ -0,0 +1,167 @@
+//  Chap 1 (Alerts)
+
+// Question no 1.
+
+alert(" First Name \n Lasnt Name \n Email \n Phone Number \n Password")
+
+// Question no 2.
+
+alert("You're learning JavaScript!");
+
+// Question no 3.
+
+alert("Hi, This is java script.")
+
+//  Chap 2 (Variables for string)
+
+
+// Question no 1.
+
+var firstName = "Asif";
+
+alert(firstName)
+
+// Question no 2.
+
+var name;
+
+name = "asif"
+
+alert(name)
+
+// Question no 3.
+
+var teamName = "Team 1";
+
+alert(teamName)
+
+// Question no 4.
+
+var bestMan = "Charlie";
+
+bestMan = "Tom"
+
+alert(bestMan)
+
+// Chapter 3 (Variables for numbers)
+
+// Question no 1.
+
+var caseQty;
+
+// Question no 2.
+
+caseQty = 144
+
+// Question no 3.
+
+var num = 9;
+
+// Question no 4.
+
+var total = caseQty + num
+
+alert(total)
+
+// Question no 5.
+
+
+var orderTotal;
+var merchTotal = 200;
+var shippingCharge = 20;
+var orderTotal = merchTotal + shippingCharge;
+
+console.log(orderTotal)
+
+// Question no 6.
+
+var Number = 89
+
+Number = Number + 90
+
+console.log(Number)
+
+// Chapter 4 (Variable names Legal and illegal)
+
+// Question no 1.
+
+var product_cost = 3.45;
+
+// Question no 2.
+
+var nameOfband;
+
+
+// Question no 3.
+
+var num = 90;
+
+
+// Question no 4.
+
+var firstname = "asif "
+
+var lastname = "khadim"
+
+var  fullname =  firstname + lastname
+
+console.log(fullname)
+
+// Question no 5.
+
+// Legal variables.
+
+// firstName, lastName, Number, name, etc.
+
+// Illegal variables.
+
+// Var, .name, if, else, while
+
+// Chapter no 5.
+
+
+// Question no 1
+
+var remainder = 10 % 3; // remainder
+
+// Question no 2
+
+var num = 20 % 6; //   2   //
+
+console.log(num)
+
+
+// Question no 3.
+
+var largeNum = 1000 * 2000;   //  2000000 //
+
+console.log(largeNum)
+
+
+// Question no 4.
+
+
+var variable1 = 10;
+var variable2 = 5;
+var result = variable1 - variable2;     //  5   //
+
+console.log(result)
+
+// Question no 5.
+
+var dividend = 15;
+var divisor = 4;
+var remainderResult = dividend % divisor;   //  3   //
+
+console.log(remainderResult)    
+
+// Question no 6.
+
+var num1 = 5;
+var num2 = 7;
+var result = num1 * num2;
+
+alert("The result of the multiplication is: " + result);
+
+
+            //              T   H   E       E   N   D       //
